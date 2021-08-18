9d09a181354bc7fc275ac51419149806cc3926f9
ercruz64 <ercruz@misena.edu.co>
2021-08-17 22:44:26 -0500

    firts commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>COL-OFFSET</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	
+	<div class="container">
+	<div class="row">
+		<div class="col-12 header">HEADER</div>
+	</div>
+
+	<div class="row">
+		<div class="col-xs-12 col-md-4 cont1">Contenido 1</div>
+		<div class="col-xs-12 col-md-8">
+			<div class="row">
+				<div class="cont2">Contenido 2</div>
+				<div class="col-12">
+					<div class="row">
+						<div class="col-sm-12 col-md-6 cont3">Cont 3</div>
+						<div class="col-sm-12 col-md-6 cont4">cont 4</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-6 col-sm-4 col-md-3 azul">5</div>
+						<div class="col-xs-6 col-sm-4 col-md-3 azul">6</div>
+						<div class="col-xs-6 col-sm-4 col-md-3 azul">7</div>
+						<div class="col-xs-6 col-sm-4 col-md-3 azul">8</div>
+						<div class="col-xs-6 col-sm-4 col-md-3 azul">9</div>
+						<div class="col-xs-6 col-sm-4 col-md-3 azul">10</div>
+						<div class="col-xs-6 col-sm-4 col-md-3 azul">11</div> 
+						<div class="col-xs-6 col-sm-4 col-md-3 azul">12</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12 footer">footer</div>
+	</div>
+	</div>
+
+	<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ offset.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<title>Document</title>
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="offset-8 col-4 columna">
+				columna de 4
+			</div>
+		</div>		
+		<div class="row">
+			<div class="col-4 columna">
+				columna de 4
+			</div>
+			<div class="col-4 offset-md-4  columna">
+				columna de 4
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-2 offset-1 columna">1</div>
+			<div class="col-2 columna">2</div>
+			<div class="col-2 columna">3</div>
+			<div class="col-2 columna">4</div>
+			<div class="col-2 columna">5</div>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
